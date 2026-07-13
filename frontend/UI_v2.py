8ba2540787ea2bc66fb0f8ce3bb6f2a87a61cd43
priya5056 <146319574+priya5056@users.noreply.github.com>
2026-07-13 13:05:57 +0530

    Update UI_v2.py

--- frontend/UI_v2.py
+++ frontend/UI_v2.py
@@ -8,8 +8,7 @@ import requests
 try:
     BACKEND_URL = st.secrets["BACKEND_URL"]
 except:
-    BACKEND_URL = "http://127.0.0.1:8000"
-
+    BACKEND_URL = "https://rag-production-1d6e.up.railway.app"
 # ----------------------------------
 # Page Config
 # ----------------------------------
