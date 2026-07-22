6fe57e92744a37e9c46be768b07a08d51c56ac28
pp <146319574+priya5056@users.noreply.github.com>
2026-07-22 11:58:37 +0530

    Update UI_v2.py

--- frontend/UI_v2.py
+++ frontend/UI_v2.py
@@ -8,7 +8,7 @@ import requests
 try:
     BACKEND_URL = st.secrets["BACKEND_URL"]
 except:
-    BACKEND_URL = "https://self-healing-two-production-0c2e.up.railway.app"
+    BACKEND_URL = "https://self-healing-two-production-2677.up.railway.app/"
 # ----------------------------------
 # Page Config
 # ----------------------------------
